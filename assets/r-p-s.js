82b8d3ec4e70b62de1cedc43915ca454dc35ad89
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-02-23 15:15:17 +0300

    add function of randong number

--- assets/r-p-s.js
+++ assets/r-p-s.js
@@ -1,45 +1,17 @@
-
 let randomNumber = Math.random();
 
- function getComputerChoice() {
-  
-    if ( randomNumber >= 0 && randomNumber < 1 /3){
+function getComputerChoice () {
     
-    return  'rock';
-    }
-    else if (randomNumber >= 1 /3 && randomNumber < 2 / 3){
-        return 'paper';
-    }
-    else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        return 'scissors';
-    }
- 
-
-function playGame (){
-
-for (i=1; i <= 5; i++){
-
-function playRound(playerSelection,computerSelection){
-   if (playerSelection = prompt('Your choice','') ){
-
-return `You lose!  ${computerSelection} beats ${playerSelection}`;
-   }
-  else if (playerSelection = 'rock' ){
-
-return `You lose!  ${computerSelection} beats ${playerSelection}`;
-   }
-   else if (playerSelection = 'scissors' ){
-
-return `You lose! ${computerSelection} beats ${playerSelection}`;
-   }
-else if (computerSelection = playerSelection){
-   return 'you tied';
+if (randomNumber > 0 && randomNumber < 1 / 3){
+    return 'rock';
 }
+else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+    return 'paper';
+}
+else if (randomNumber >= 2 / 3 && randomNumber < 1){
+    return 'scissors';
 }
 
-const playerSelection = prompt('Your choice',''); 
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
 }
-}
- }
+
+console.log(getComputerChoice());
