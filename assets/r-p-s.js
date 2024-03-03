c667c9d024cc3f958ed12ebe12347cc60ce94e63
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 22:46:35 +0300

    add get computerChoice fn

--- assets/r-p-s.js
+++ assets/r-p-s.js
@@ -1,2 +1,17 @@
+ let randomNumber = Math.random();
  
+ function getComputerChoice(){
+
+    if (randomNumber > 0 && randomNumber < 1 / 3){
+        return 'rock'
+    }
+    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+        return 'paper';
+    }
+    else if (randomNumber >= 2 / 3 && randomNumber < 1 ){
+        return 'scissors';
+    }
+
+ }
+ console.log(getComputerChoice());
 
